8eac0de58bab851ff387b754c413886ede0964a0
Dungna89 <dungna29@fpt.edu.vn>
2022-01-05 17:42:00 +0700

    1

---
+++ 1. BÀI TẬP CSDL/FinalAssignment2021 - Phần 2.sql
@@ -0,0 +1,48 @@
+﻿-- FINAL ASSIGNMENT COM2012 DUNGNA29 --
+
+/*
+1.0 Tạo ra 1 View báo cáo doanh số sản phẩm bao gồm các cột thông tin sau để báo cáo cho giám đốc của đại lý sấp xếp giảm dần theo Số lượng đã bán:
+[Mã Sản Phẩm] [Tên Sản Phẩm] [Mã Dòng Sản phẩm] [Tên Dòng Sản phẩm] [Số Lượng Tồn Kho] [Số Lượng Đã Bán] [Số tiền lãi] 
+-- sanpham - dongsanpham - hoadonchitiet */
+
+
+/* 1.1 Công ty quyết định khen thưởng cho tất cả các nhân viên bán được từ 3 sản phẩm trở lên sẽ tăng lương thêm 1.000.000 - Sử dụng câu lệnh cập nhật
+*/
+
+/*
+1.2 Tạo ra 1 View báo cáo cho thanh tra của tập đoàn xuống kiểm tra gồm những cột sau và sắp xếp theo tổng số lượng sản phẩm đã bán của nhân viên đó
+[Tên Cửa Hàng][Thành Phố][Địa Chỉ 1][Quốc Gia][Mã Nhân Viên] [Tên Nhân Viên] [Số điện thoại] [Lương] [Mã Người Báo Cáo] [Tên người Báo Cáo][Chức danh người báo cáo][Tổng số lượng sản phẩm đã bán của nhân viên đó]
+*/
+
+
+
+/*
+1.3 Sắp tới 30/4 mùng 1/5 đang có chương trình tặng quà cho những khách hàng từng mua hàng tổng đơn hàng từ 15 triệu trở lên. Hãy tạo 1 View hiển thị những khách hàng nằm trong diện được thưởng bao gồm các cột và sắp xếp theo tổng số tiền đã mua
+[Id Khách Hàng] [Họ và Tên Khách Hàng] [Số điện thoại] [Địa Chỉ 1] [Thành phố] [Tổng số lượng hà đã mua] [Tổng số tiền đã mua]
+*/ 
+
+
+/*
+1.4 Hiển thị ra 1 View báo cáo Các hóa đơn có tình trạng chưa ship hàng cho khách được và để các trưởng phòng họp bắt các nhân viên giải trình hiển thị ra các cột như sau và sắp xếp giảm dần theo số lượng:
+[ID Hóa Đơn] [Mã Nhân Viên] [Tên Nhân Viên] [Ngày Lập Hóa Đơn] [Ngày Giao Hàng] [Tên Khách Hàng] [Số ĐT Khách Hàng] [Quận] [Trạng Thái Hóa ĐƠn] [Số Lượng trên đơn]
+*/-- Nhóm Q làm chưa chính xác câu này vì không GroupBy
+
+
+/*
+1.5 Hiển thị ra 1 View báo cáo các sản phẩm không bán chạy để phòng marketing tiến hành các chiến lược đẩy mạnh doanh số và sắp xếp theo thứ tự tăng dần theo cột số lượng đã bán:
+[Tên Dòng sản phẩm] [Website] [Mã Dòng Sản phẩm] [Tên Sản Phẩm] [Giá Nhâm] [Mô Tả] [Trọng Lượng] [Số lượng Tồm Kho] [Số lượng đã bán]
+*/ -- Nhóm Huy Quyết làm câu này chưa chính xác vì không group
+
+      
+/*
+1.6 Liệt kê các nhân viên có chức danh không phải nhân viên mà là từ phó phòng trở lên để gửi email họp toàn quốc vào mùa đại dịch Covid gồm các cột sau:
+[Mã Nhân Viên] [Tên Nhân Viên] [Tên Chức Danh] [Email] [Số điện thoại] [Tên Cửa Hàng] [Thành phố] [Địa Chỉ 1 Cửa Hàng]
+*/
+
+/*
+1.7 Tạo ra một View báo cáo tình trạng các hóa đơn còn chậm chưa giao cho khách và hiển thị ra cột số ngày chậm so với ngày dự kiến và gồm các cột sau:
+[ID Hóa Đơn] [Mã Nhân Viên] [Tên Nhân Viên] [Ngày Lập Hóa Đơn] [Ngày Giao Hàng] [Số Ngày Chậm] [Tên Khách Hàng] [Số ĐT Khách Hàng][Tên Cửa Hàng]
+*/
+
+
+
